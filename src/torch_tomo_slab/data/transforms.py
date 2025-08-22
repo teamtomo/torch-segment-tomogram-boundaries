67908cec96a326008dec0f6049c7ee83ef05e823
shahpnmlab <34034134+shahpnmlab@users.noreply.github.com>
2025-08-23 00:06:26 +0100

    fewer augmentations

--- src/torch_tomo_slab/data/transforms.py
+++ src/torch_tomo_slab/data/transforms.py
@@ -109,12 +109,18 @@ def get_transforms(is_training: bool = True, use_balanced_crop: bool = True) -> 
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
             A.RandomBrightnessContrast(brightness_limit=constants.AUGMENTATION_CONFIG['BRIGHTNESS_CONTRAST_LIMIT'], contrast_limit=constants.AUGMENTATION_CONFIG['BRIGHTNESS_CONTRAST_LIMIT'], p=0.4),
-            A.GaussNoise(std_range=constants.AUGMENTATION_CONFIG['GAUSS_NOISE_STD_RANGE'], p=0.4),
-            A.GaussianBlur(blur_limit=constants.AUGMENTATION_CONFIG['GAUSS_BLUR_LIMIT'], p=0.4),
-            A.ElasticTransform(p=0.3, alpha=constants.AUGMENTATION_CONFIG['ELASTIC_ALPHA'], 
-                              sigma=constants.AUGMENTATION_CONFIG['ELASTIC_SIGMA']),
             A.CoarseDropout(p=0.4, num_holes_range=(1,12), hole_height_range=(0.05,0.25), hole_width_range=(0.05,0.25), fill=0),
         ]
+
+        if 'GAUSS_NOISE_STD_RANGE' in constants.AUGMENTATION_CONFIG:
+            transform_list.append(A.GaussNoise(std_range=constants.AUGMENTATION_CONFIG['GAUSS_NOISE_STD_RANGE'], p=0.4))
+
+        if 'GAUSS_BLUR_LIMIT' in constants.AUGMENTATION_CONFIG:
+            transform_list.append(A.GaussianBlur(blur_limit=constants.AUGMENTATION_CONFIG['GAUSS_BLUR_LIMIT'], p=0.4))
+
+        if 'ELASTIC_ALPHA' in constants.AUGMENTATION_CONFIG and 'ELASTIC_SIGMA' in constants.AUGMENTATION_CONFIG:
+            transform_list.append(A.ElasticTransform(p=0.3, alpha=constants.AUGMENTATION_CONFIG['ELASTIC_ALPHA'],
+                                                  sigma=constants.AUGMENTATION_CONFIG['ELASTIC_SIGMA']))
         
         # Add balanced crop or regular random crop
         if use_balanced_crop:
